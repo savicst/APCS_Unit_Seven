5f6f48d6a11972a8852f4d21a8998ce78045a50d
savicst <savicst@ssfs.org>
2022-01-27 15:15:29 -0500

    started abnvwklsbre

--- src/ArrayListMethods.java
+++ src/ArrayListMethods.java
@@ -3,32 +3,45 @@ import java.util.Scanner;
 
 public class ArrayListMethods {
 
-    /*
-    public static ArrayList<Integer> numberList() {
 
+    public static ArrayList<Integer> numberList() {
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        numList.add(0);
+        numList.add(-5);
+        numList.add(7);
+        numList.add(12);
+        numList.add(-1);
+        numList.add(3,10);
+        numList.set(1,9);
+        numList.remove(4);
+        return numList;
 
     }
 
 
 
     public static ArrayList<Double> firstAndLast() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Next number: ");
+        ArrayList<Double> matt = new ArrayList<Double>();
+        while(scan.nextDouble() != 0){
+            matt. =
+    }
+
+
+
+   // public static boolean double23(ArrayList<Integer> nums) {
+
 
     }
 
 
 
-    public static boolean double23(ArrayList<Integer> nums) {
+
+    //public static boolean commonEnd(ArrayList<Integer> a, ArrayList<Integer> b) {
 
 
-    }
+   // }
 
-
-
-
-    public static boolean commonEnd(ArrayList<Integer> a, ArrayList<Integer> b) {
-
-
-    }
-*/
 
 }
