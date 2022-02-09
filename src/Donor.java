43ed534854bfd3414a8e13ab8db10bf950be4375
savicst <savicst@ssfs.org>
2022-02-09 08:55:45 -0500

    started donor

--- src/Donor.java
+++ src/Donor.java
@@ -1,4 +1,16 @@
 public class Donor {
-
-
+    private String name;
+    private double amount;
+//constructor
+    public Donor(String n, double a){
+        name = n;
+        amount = a;
+    }
+//getters
+    public String getName() {
+        return name;
+    }
+    public double getAmount() {
+       return amount;
+    }
 }
--- src/DonorRunner.java
+++ src/DonorRunner.java
@@ -11,8 +11,8 @@ public class DonorRunner {
         donorList = new ArrayList<>();
         getDonorList("files/donors.txt");
         sortDonorList();
-        //System.out.println(donorList.get(0).getName()); // Should print out Hernandez
-        //System.out.println(donorList.get(donorList.size()-1).getName()); // Should print out Hall
+        System.out.println(donorList.get(0).getName()); // Should print out Hernandez
+        System.out.println(donorList.get(donorList.size()-1).getName()); // Should print out Hall
         System.out.println(getDonatedAmount("Anderson")); // should print 1436.5
         System.out.println(getDonatedAmount("Roberts")); // should print 307.86
         System.out.println(getDonatedAmount("Wilkinson")); // should print 0.0
@@ -20,11 +20,26 @@ public class DonorRunner {
     }
 
     public static void getDonorList(String filename) throws IOException {
-
+        Scanner input = new Scanner(new File(filename));
+       while(input.hasNext()) {
+           String name = input.next();
+           double amount = input.nextDouble();
+           Donor nextDonor = new Donor(name, amount);
+           donorList.add(nextDonor);
+       }
     }
 
     public static void sortDonorList() {
-
+        for(int j=1; j<donorList.size(); j++){
+            Donor tempDonor = donorList.get(j);
+            double temp = donorList.get(j).getAmount();
+            int i = j-1;
+            while (i>=0 && temp < donorList.get(i).getAmount()){
+                donorList.set(i+1, donorList.get(i));
+                i--;
+            }
+            donorList.set(i+1,tempDonor);
+        }
     }
 
     public static double getDonatedAmount(String name) {
