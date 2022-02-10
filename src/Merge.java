ea2e233b9c34918b119616724d22e52d9bbc689a
savicst <savicst@ssfs.org>
2022-02-10 12:25:14 -0500

    completed Merge :)

--- src/Merge.java
+++ src/Merge.java
@@ -2,10 +2,30 @@ import java.util.ArrayList;
 
 public class Merge {
 
-    /*
-    public static ArrayList<Integer> mergeLists(ArrayList<Integer> listOne, ArrayList<Integer> listTwo) {
 
+    public static ArrayList<Integer> mergeLists(ArrayList<Integer> listOne, ArrayList<Integer> listTwo) {
+        ArrayList<Integer> comboList = new ArrayList();
+        while (listOne.size() != 0 && listTwo.size() != 0) {
+
+            if (listOne.get(0) < listTwo.get(0)) {
+                comboList.add(listOne.get(0));
+                listOne.remove(0);
+            }
+            else {
+                comboList.add(listTwo.get(0));
+                listTwo.remove(0);
+            }
+        }
+        while (listOne.size() != 0) {
+            comboList.add(listOne.get(0));
+            listOne.remove(0);
+        }
+        while (listTwo.size() != 0) {
+            comboList.add(listTwo.get(0));
+            listTwo.remove(0);
+        }
+    return comboList;
     }
 
-     */
+
 }
