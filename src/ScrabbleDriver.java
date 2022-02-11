36b920073337a46015d9470b2d8de52168cfadff
savicst <savicst@ssfs.org>
2022-02-11 10:59:55 -0500

    started scrabbling

--- src/ScrabbleDriver.java
+++ src/ScrabbleDriver.java
@@ -1,7 +1,28 @@
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ScrabbleDriver {
+    //1.4
+    public static void main (String[] args) throws FileNotFoundException {
+        ScrabbleHelper matthias = new ScrabbleHelper();
+        System.out.println(matthias.getWordList().get(66349));
+    //3
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a word to test: ");
+        while(true){
+            String input= scan.next();
+            if (input.equals("")){
+            break;}
+            if (matthias.foundWord(input)){
+                System.out.println("\"");
+            }
+            System.out.println("Enter a word to test: ");
+
+        }
+
+    }
 
 
 }
--- src/ScrabbleHelper.java
+++ src/ScrabbleHelper.java
@@ -7,6 +7,34 @@ import java.util.Scanner;
 
 public class ScrabbleHelper {
 
+    //1.1
+    private ArrayList<String> wordList = new ArrayList<>();
+    //1.2
+    public ScrabbleHelper() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("files/enable.txt"));
+        while (input.hasNext()){
+            wordList.add(input.next());
+        }
+    }
+    //1.3
+    public ArrayList<String> getWordList() {
+        return wordList;
+    }
+    //2
+    public boolean foundWord(String word){
+        int min = 0;
+        int max = wordList.size() - 1;
+        while (min <= max) {
+            int mid = (min + max) / 2;
+            if (wordList.get(mid).compareTo(word) == 0) {
+                return true;
+            } else if (wordList.get(mid).compareTo(word) > 0) {
+                max = mid - 1;
+            } else
+                min = mid + 1;
+        }
+        return false;
 
+    }
 }
 
