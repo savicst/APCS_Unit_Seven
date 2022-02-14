3d1145836ddc4bb0974f4dccebf58bcf4dbf7b6c
savicst <savicst@ssfs.org>
2022-02-14 11:24:42 -0500

    scrabbled

--- src/ScrabbleDriver.java
+++ src/ScrabbleDriver.java
@@ -4,24 +4,31 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class ScrabbleDriver {
-    //1.4
+    //1-1.4
     public static void main (String[] args) throws FileNotFoundException {
         ScrabbleHelper matthias = new ScrabbleHelper();
         System.out.println(matthias.getWordList().get(66349));
-    //3
+    //1-3
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter a word to test: ");
         while(true){
-            String input= scan.next();
+            String input= scan.nextLine();
             if (input.equals("")){
             break;}
             if (matthias.foundWord(input)){
-                System.out.println("\"");
+                System.out.println("\"" + input + "\"" + " is a word");
+            }
+            else{
+                System.out.println("\"" + input + "\"" + " is not a word");
             }
             System.out.println("Enter a word to test: ");
-
         }
-
+        System.out.println("Good-Bye!");
+        //2-3
+        ArrayList<String> matched = matthias.findMatches("for");
+        System.out.println(matched);
+        matthias.sortWords(matched);
+        System.out.println(matched);
     }
 
 
--- src/ScrabbleHelper.java
+++ src/ScrabbleHelper.java
@@ -7,20 +7,20 @@ import java.util.Scanner;
 
 public class ScrabbleHelper {
 
-    //1.1
+    //1-1.1
     private ArrayList<String> wordList = new ArrayList<>();
-    //1.2
+    //1-1.2
     public ScrabbleHelper() throws FileNotFoundException {
         Scanner input = new Scanner(new File("files/enable.txt"));
         while (input.hasNext()){
             wordList.add(input.next());
         }
     }
-    //1.3
+    //1-1.3
     public ArrayList<String> getWordList() {
         return wordList;
     }
-    //2
+    //1-2
     public boolean foundWord(String word){
         int min = 0;
         int max = wordList.size() - 1;
@@ -34,7 +34,28 @@ public class ScrabbleHelper {
                 min = mid + 1;
         }
         return false;
-
+    }
+    //2-1 and 2-2
+    public ArrayList<String> findMatches(String pattern){
+        ArrayList<String> matches = new ArrayList<String>();
+         for (int i=0;i<wordList.size(); i++){
+             if (wordList.get(i).indexOf(pattern) >=0){
+                 matches.add(wordList.get(i));
+             }
+         }
+         return matches;
+    }
+    //2-4
+    public void sortWords(ArrayList<String> unsorted){
+        for( int j=0; j<unsorted.size()-1;j++){
+            int minIndex = j;
+            for (int k = j+1; k<unsorted.size();k++){
+                if(unsorted.get(k).length()< unsorted.get(minIndex).length()){
+                    minIndex = k;
+                }
+            }
+            int temp =
+        }
     }
 }
 
